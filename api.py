1dd733a35514a3a2474b2571e81c0f8079a174fb
Emmanuel KOUPOH <emmanuelkoupoh@gmail.com>
2023-05-04 15:05:05 +0000

    api

--- api.py
+++ api.py
@@ -1,4 +1,60 @@
 from fastapi import FastAPI
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+from typing import List,  Literal
+from pydantic import BaseModel
+from scipy.special import softmax
+
+# Setup
+model_path = f"GhylB/Sentiment_Analysis_DistilBERT"
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+config = AutoConfig.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+# Functions
+
+# Preprocess text (username and link placeholders)
+
+
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+
+def sentiment_analysis(text):
+    text = preprocess(text)
+
+    # PyTorch-based models
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores_ = output[0][0].detach().numpy()
+    scores_ = softmax(scores_)
+
+    # Format output dict of scores
+    labels = ['Negative', 'Neutral', 'Positive']
+    scores = {l: float(s) for (l, s) in zip(labels, scores_)}
+
+    return scores
+
+# INPUT MODELING
+class ModelInput(BaseModel):
+    """Modeling of one input data in a type-restricted dictionary-like format
+
+    column_name : variable type # strictly respect the name in the dataframe header.
+
+    eg.:
+    =========
+    customer_age : int
+    gender : Literal['male', 'female', 'other']
+    """
+
+    tweet : str
+
 
 app = FastAPI()
 
@@ -6,3 +62,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/tweet")
+async def run(input: ModelInput):
+    
+    result = sentiment_analysis(text=input.tweet)
+
+    return {
+        "input_text": input.tweet,
+        "confidence_scores":result
+    }
